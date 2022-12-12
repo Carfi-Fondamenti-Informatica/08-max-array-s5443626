30ed2708e223260a03d4036631d5ca61e71e32e9
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-12 15:52:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  float x=0;
+  int n=0
+    float dimensione[n];
+    for(int i=0;i<n;i++){
+        cin >> dimensione [i];
+    }
+   for (int i=0;i<n;i++){
+       float d=max(dimensione[1],dimensione[i-1]);
+       if (d>x){
+           x=d;
+       }
+   }cout << x;
   return 0;
 }
